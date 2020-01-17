598946601f622eca5298d88a31c73db8d03ffda8
Higor Roberto <h.saposilva@gmail.com>
2020-01-17 16:09:47 -0300

    JavaScrip e Typescript

---
+++ 02-typescript/main.ts
@@ -0,0 +1,21 @@
+var minhavar = 'minha variavel';
+
+function minhaFunc(x, y) {
+    return x + y;
+}
+
+let num = 5;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+
+numeros.map(valor => valor * 2);
+
+class Matematica {
+    soma(x, y) {
+        return x + y;
+    }
+}
+
+var n: string = "Higor";
+n = "Higor Roberto";
